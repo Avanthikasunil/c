8edf1bee2736d63e34dddac1887fd60fc05f7059
Avanthikasunil <avanthikasunil04@gmail.com>
2026-09-10 11:02:44 +0530

    Add files via upload

---
+++ fact.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+  2 int main()
+  3 {
+  4     int n, fact = 1;
+  5     printf("enter the number:");
+  6     scanf("%d",&n);
+  7     for (int i=1; i<=n; i++)
+  8     {
+  9     fact = fact * i;
+ 10     }
+ 11     printf("factorial = %d",fact);
+ 12     return 0;
+ 13 }
+
+
+
+
+output:-
+
+enter the number:3
+factorial = 6
